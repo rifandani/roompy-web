80afdc913920f5b9d2054683e1653cc5e8bdc8f1
rifandani <ipandani2505@gmail.com>
2020-12-17 23:43:32 +0700

    :sparkles: NEW: login functionality

--- src/pages/login.tsx
+++ src/pages/login.tsx
@@ -1,19 +1,55 @@
 import Link from 'next/link';
+import { MouseEvent, useState } from 'react';
+import validator from 'validator';
+import { toast } from 'react-toastify';
+import { useRouter } from 'next/router';
 // files
+import { auth } from '../configs/firebaseConfig';
 
 export default function LoginPage() {
-  async function login() {
-    alert('login');
+  // state
+  const [email, setEmail] = useState<string>('');
+  const [password, setPassword] = useState<string>('');
+
+  // useRouter
+  const { push } = useRouter();
+
+  async function login(e: MouseEvent) {
+    e.preventDefault();
+
+    // validation
+    if (!email || !password)
+      return toast.warning('Please input all required field');
+    if (!validator.isEmail(email))
+      return toast.warning('Please input a valid email');
+    if (!validator.isLength(password, { min: 6 }))
+      return toast.warning('Please input min 6 chars PASSWORD');
+
+    try {
+      const credential = await auth.signInWithEmailAndPassword(email, password);
+
+      toast.success(`Welcome Back, ${credential.user.displayName}`);
+      return push('/');
+    } catch (err) {
+      // Handle Errors here.
+      if (err.code === 'auth/wrong-password') {
+        toast.error('Wrong password.');
+      } else {
+        toast.error(err.message);
+      }
+
+      return console.error(err);
+    }
   }
 
   return (
     // <!-- Page Container -->
-    <div className="pt-16 min-h-screen flex justify-center bg-gray-100 text-gray-900 relative">
+    <div className="relative flex justify-center min-h-screen pt-16 text-gray-900 bg-gray-100">
       {/* <!-- Waves Background --> */}
-      <div className="absolute right-0 bottom-0 left-0">
+      <div className="absolute bottom-0 left-0 right-0">
         <svg viewBox="0 -100 1440 320">
           <path
-            className="fill-current text-purple-200"
+            className="text-purple-200 fill-current"
             fillOpacity="1"
             d="M0,96L26.7,112C53.3,128,107,160,160,181.3C213.3,203,267,213,320,192C373.3,171,427,117,480,122.7C533.3,128,587,192,640,229.3C693.3,267,747,277,800,245.3C853.3,213,907,139,960,138.7C1013.3,139,1067,213,1120,224C1173.3,235,1227,181,1280,154.7C1333.3,128,1387,128,1413,128L1440,128L1440,320L1413.3,320C1386.7,320,1333,320,1280,320C1226.7,320,1173,320,1120,320C1066.7,320,1013,320,960,320C906.7,320,853,320,800,320C746.7,320,693,320,640,320C586.7,320,533,320,480,320C426.7,320,373,320,320,320C266.7,320,213,320,160,320C106.7,320,53,320,27,320L0,320Z"
           ></path>
@@ -22,42 +58,42 @@ export default function LoginPage() {
       {/* <!-- END Waves Background --> */}
 
       {/* <!-- Log In Section --> */}
-      <div className="px-5 py-6 lg:px-6 lg:py-8 w-full md:w-8/12 lg:w-6/12 xl:w-4/12 relative">
+      <div className="relative w-full px-5 py-6 lg:px-6 lg:py-8 md:w-8/12 lg:w-6/12 xl:w-4/12">
         {/* <!-- Logo --> */}
         <div className="mb-6 text-center">
           <Link href="/">
             <div className="flex items-center justify-center cursor-pointer">
-              <img className="w-10 inline-block mr-2" src="favicon.ico" />
+              <img className="inline-block w-10 mr-2" src="favicon.ico" />
 
-              <h3 className="text-3xl font-bold inline-flex items-center text-purple-700">
+              <h3 className="inline-flex items-center text-3xl font-bold text-purple-700">
                 Roompy
               </h3>
             </div>
           </Link>
 
-          <p className="mt-2 italic text-sm text-gray-500">
+          <p className="mt-2 text-sm italic text-gray-500">
             Please login to your account
           </p>
         </div>
         {/* <!-- END Logo --> */}
 
         {/* <!-- Form --> */}
-        <div className="rounded border p-6 lg:p-8 shadow-sm bg-white">
+        <div className="p-6 bg-white border rounded shadow-sm lg:p-8">
           <form>
-            <label className="text-sm block text-gray-700">
+            <label className="block text-sm text-gray-700">
               Email
               <input
-                className="appearance-none rounded-md px-4 py-3 mt-1 block w-full outline-none border-b-2 hover:border-purple-700 hover:shadow-xl focus:border-purple-700"
+                className="block w-full px-4 py-3 mt-1 border-b-2 rounded-md outline-none appearance-none hover:border-purple-700 hover:shadow-xl focus:border-purple-700"
                 placeholder="elonmusk@email.com"
                 type="email"
                 required
               />
             </label>
 
-            <label className="text-sm block text-gray-700 mt-4">
+            <label className="block mt-4 text-sm text-gray-700">
               Password
               <input
-                className="appearance-none rounded-md px-4 py-3 mt-1 block w-full outline-none border-b-2 hover:border-purple-700 hover:shadow-xl focus:border-purple-700"
+                className="block w-full px-4 py-3 mt-1 border-b-2 rounded-md outline-none appearance-none hover:border-purple-700 hover:shadow-xl focus:border-purple-700"
                 placeholder="******"
                 type="password"
                 required
@@ -67,26 +103,26 @@ export default function LoginPage() {
 
             <div className="mt-6">
               <button
-                className="px-4 py-3 font-bold tracking-wider uppercase rounded-md focus:outline-none focus:shadow-outline block w-full hover:text-purple-700 hover:bg-purple-100 text-white bg-purple-700"
+                className="block w-full px-4 py-3 font-bold tracking-wider text-white uppercase bg-purple-700 rounded-md focus:outline-none focus:shadow-outline hover:text-purple-700 hover:bg-purple-100"
                 type="submit"
-                onClick={login}
+                onClick={(e) => login(e)}
               >
                 Login
               </button>
             </div>
           </form>
 
-          <div className="mt-6 text-center text-sm">
+          <div className="mt-6 text-sm text-center">
             <Link href="/register">
-              <a className="italic text-gray-500 block md:inline-block mb-2 md:mb-0 hover:text-purple-700 hover:underline">
+              <a className="block mb-2 italic text-gray-500 md:inline-block md:mb-0 hover:text-purple-700 hover:underline">
                 Register
               </a>
             </Link>
 
-            <span className="text-gray-500 mx-1 hidden md:inline-block">·</span>
+            <span className="hidden mx-1 text-gray-500 md:inline-block">·</span>
 
             <Link href="/forgot-password">
-              <a className="italic text-gray-500 block md:inline-block hover:text-purple-700 hover:underline">
+              <a className="block italic text-gray-500 md:inline-block hover:text-purple-700 hover:underline">
                 Forgot Password?
               </a>
             </Link>
